7f24a2d06bbc7e1a5f73184437b78e69b0f6af53
cleiton <cleitonjrti@gmail.com>
2019-10-02 10:17:33 -0300

    Added cors to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
@@ -15,6 +16,7 @@ const dbConfig = {
 
 mongoose.connect(dbUrl, dbConfig);
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
