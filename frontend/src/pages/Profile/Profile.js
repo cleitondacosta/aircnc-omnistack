2ed08ad4fdcebbe9e590deef855bf03f3e7d773c
cleiton <cleitonjrti@gmail.com>
2019-10-05 21:15:16 -0300

    Added socket io client in frontend

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -1,11 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import socketIOClient from 'socket.io-client';
 
 import './Profile.css';
 
 export default function Profile() {
   const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
+
+  const loggedUserId = localStorage.getItem('user');
+  const socket = useMemo(() => {
+    return socketIOClient(
+      'http://10.0.0.14:3333', {
+        query: { user_id: loggedUserId }
+      }
+    );
+  }, [loggedUserId]);
+
+
+  useEffect(() => {
+    socket.on('booking request', data => {
+      setRequests([...requests, data]);
+    });
+  }, [requests, socket]);
+
 
   useEffect(() => {
     async function loadSpots() {
@@ -40,8 +59,50 @@ export default function Profile() {
     });
   }
 
+  async function handleAccept(id) {
+    await api.post(`/booking/${id}/approval`);
+
+    setRequests(
+      requests.filter(request => request._id !== id)
+    );
+  }
+
+  async function handleReject(id) {
+    await api.post(`/booking/${id}/rejection`);
+
+    setRequests(
+      requests.filter(request => request._id !== id)
+    );
+  }
+
   return (
     <>
+      <ul className="notification">
+        {requests.map(request => {
+          return <li key={request._id}>
+            <p>
+              <strong>{request.user.email} </strong>
+              is requesting a booking in
+              <strong> {request.spot.company} </strong>
+              , on date <strong>{request.date}</strong>
+            </p>
+
+            <button 
+              className="accept" 
+              onClick={() => handleAccept(request._id)}
+            >
+              ACCEPT
+            </button>
+            <button 
+              className="reject" 
+              onClick={() => handleReject(request._id)}
+            >
+              DECLINE
+            </button>
+          </li>
+        })}
+      </ul>
+
       <ul className="spot-list">
         {renderSpots()}
       </ul>
